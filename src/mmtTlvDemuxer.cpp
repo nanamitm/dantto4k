4d056f8986d64bfbb83202df6ab196ceade96b28
nanamitm <104249706+nanamitm@users.noreply.github.com>
2026-06-28 08:17:23 +0900

    Revert "Skip decodedDumpCallback when ACAS decryption fails"
    This reverts commit 0fcb7ef533e0952a980a6eb2f0c938228e92bc86.

--- src/mmtTlvDemuxer.cpp
+++ src/mmtTlvDemuxer.cpp
@@ -238,6 +238,10 @@ DemuxStatus MmtTlvDemuxer::demux(Common::ReadStream& stream) {
                         if (decodedDumpErrorCallback) {
                             decodedDumpErrorCallback();
                         }
+                        if (decodedDumpCallback) {
+                            auto packet = serializeTlvPacket(static_cast<uint8_t>(TlvPacketType::HeaderCompressedIpPacket), tlv.getData());
+                            decodedDumpCallback(packet.data(), packet.size());
+                        }
                         return DemuxStatus::WattingForEcm;
                     }
                 }
